1c759214926497bcf87610dd76c67c5ef2477a13
Melody Li <ml867@cornell.edu>
2017-08-22 18:48:02 -0400

    nav bar update and edit viewing

--- app/components/App.js
+++ app/components/App.js
@@ -1,24 +1,46 @@
 var React = require('react');
+var ReactRouter = require('react-router-dom');
+var Router = ReactRouter.BrowserRouter;
+var Route = ReactRouter.Route;
+var Switch = ReactRouter.Switch;
 var Nav = require('./Nav.js');
 var Header = require('./Header.js');
 var Home = require('./Home.js');
+var TranscriptEditor = require('./TranscriptEditor.js')
+var Contact = require('./Contact.js')
 
 class App extends React.Component {
-	getLanguages() {
-		//databse call to get languages
-		return ['French', 'English', 'Russian', 'Chinese (Mandarin)', 'Portugese', 'Italian', 'Japanese', 'Onondagan', 'Persian', 'Quechua',
-		'Arabic (Standard)', 'Spanish (Mexican)', 'Spanish (Spain)', 'French (Canadian)', 'Indonesian', 'Sinhala', 'Okinawan (Shuri)', 
-		'Swahili', 'Irish', 'Hungarian', 'Yiddish', 'English (British)', 'Berber'];
+	showList(id){
+		var list = document.getElementById(id);
+		if (list.style.display == 'flex'){
+			list.style.display = 'none';
+		} else {
+			list.style.display = 'flex';
+		}
 	}
 	render () {
 		return(
-			<div className = 'app'>
-				<Nav content = {this.getLanguages()}/>
-				<div className = 'main'>
-					<Header logo = '' banner = ''/>
-					<Home />
+			<Router>
+				<div className = 'app'>
+					<Nav onSelect = {this.showList} />
+						<div className = 'main'>
+							<Header logo = '' banner = ''/>
+							<Switch>
+								<Route exact path = '/' component = {Home} />
+								<Route path = "/:lang/:name/:id" component = {TranscriptEditor} />
+								<Route path = '/contact' component = {Contact} />
+								<Route render = {function () {
+									return (
+										<div>
+											404 Error.
+										</div>
+									)
+								}} />
+							</Switch>
+						</div>
 				</div>
-			</div>
+			</Router>
+			
 		)
 	}
 }
--- app/components/Nav.js
+++ app/components/Nav.js
@@ -1,45 +1,82 @@
 var React = require('react');
 var PropTypes = require('prop-types');
 var TranscriptionNav = require('./TranscriptionNav.js');
+var NavLink = require('react-router-dom').NavLink;
 
-function Nav(props) {
-	return (
-		<div className = 'nav-wrapper'>
-			<div className = 'nav'>
-				<ul className = 'languages'>
-					<li className = 'title'> Home</li>	
-					<li className = 'title'>Languages</li>
-					{props.content.map(function(key){
-						{var str = key.replace(/\s/g, '')}
-						return (
-							<nav className = 'langNav'>
-								<ul>
-									<li key = {str}><a href = '#' onClick = {showList()}>{key}</a></li>
-									<TranscriptionNav transcripts = {getTranscripts()} lang = {str} />
-									<hr />
-								</ul>
-							</nav>
-						)
-					})}
-				</ul>
+class Nav extends React.Component {
+	constructor(props){
+		super(props);
+		this.languages = this.getLanguages();
+		this.language_info = {};
+		this.languages.map(function(item) {
+			this.language_info[item] = this.getAudioFiles(item);
+		}, this)
+		this.onSelect = props.onSelect;
+	}
+
+	getLanguages() {
+		//databse call to get languages
+		return ['French', 'English', 'Russian', 'Chinese (Mandarin)', 'Portugese', 'Italian', 'Japanese', 'Onondagan', 'Persian', 'Quechua',
+		'Arabic (Standard)', 'Spanish (Mexican)', 'Spanish (Spain)', 'French (Canadian)', 'Indonesian', 'Sinhala', 'Okinawan (Shuri)', 
+		'Swahili', 'Irish', 'Hungarian', 'Yiddish', 'English (British)', 'Berber'];
+	}
+
+	getAudioFiles(lang){
+	//database call for audio files details, parameter lang is the language group the audio file belongs to
+		return [
+		    {
+		        "id": 0,
+		        "speakerName": "John Travolta",
+		        "language": "English",
+		        "timeCreated": "1997-07-16T19:20:30"
+		    },
+		    {
+		    	"id": 1,
+		    	"speakerName": "Dave Bump",
+		    	"language": "English",
+		    	"timeCreated": "1998-09-23"
+		    }
+        ];
+ 	}
+
+	render(){ 
+		return (
+			<div className = 'nav-wrapper'>
+				<div className = 'nav'>
+					<ul className = 'languages'>
+						<li className = 'title'><NavLink exact activeClassName = 'active' to = '/'>Home</NavLink></li>	
+						<li className = 'title'><a onClick = {this.onSelect.bind(null, 'langNav')}>Languages</a></li>
+						<div id = 'langNav'>
+							{this.languages.map(function(key, index){
+								{var str = key.replace(/\s/g, '')}
+								return (
+									<div className = 'langList' key = {key}>
+										<ul>
+											<li key = {str + index}>
+												<a onClick = {this.onSelect.bind(null, str)}>
+												{key}
+												</a>
+											</li>
+											<TranscriptionNav transcripts = {this.language_info[key]} lang = {str} />
+											<hr />
+										</ul>
+									</div>
+								)
+							}, this)}
+						</div>
+						<li className = 'title'><NavLink activeClassName = 'active' to = '/contact'>Contact Us</NavLink></li>
+						<li className = 'login'><NavLink activeClassName = 'active' to = '/login'>Log In</NavLink></li>
+					</ul>
+				</div>
 			</div>
-		</div>
-	)
 
+		)
+	}
 }
 
-function getTranscripts(){
-	//database call for titles of audio files
-	return ['Interview 1', 'Interview 2'];
-}
-
-function showList(id) {
-	// var list = document.GetElementById(id);
-	// list.style.display = 'flex';
-}
 
 Nav.propTypes = {
-	content: PropTypes.array.isRequired
+	onSelect: PropTypes.func.isRequired
 }
 
 module.exports = Nav;
